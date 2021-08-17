65b2585b72a4c9adbb48ca18162911da09da3511
MohamedH1998 <mohamedahassan188@gmail.com>
2021-08-17 17:00:34 +0100

    add elixir project

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -6,6 +6,7 @@ import forkify from '../../assets/imgs/forkify.png'
 import papaya from '../../assets/imgs/papaya.png'
 import kiqs from '../../assets/imgs/kiqs.png'
 import tasktrckr from '../../assets/imgs/tasktrckr.png'
+import elixir from '../../assets/imgs/elixir.png'
 
 
 
@@ -21,22 +22,24 @@ const Portfolio = () => {
       <h2 className="section-heading">Portfolio</h2>
       <div className="projects-container">
         <Project photo={theCamperApp} cardTitle="The Camper App" sourceCode="https://github.com/MohamedH98/forkify" liveDemo="https://the-camperapp.herokuapp.com/" 
-        cardText="The Camper App is a full-stack application that enables users to
+              cardText="The Camper App is a full-stack application that enables users to
               view various campground locations from around the world and
               allows them to create their own."/>
+        <Project photo={elixir} cartTitle="Elixir" sourceCode="https://github.com/MohamedH1998/elixir" liveDemo="https://elixir-commerce.netlify.app/"
+              cardText="Elixir commerce is a react application made using React JS, Redux, OAuth (authentication)."/>
         <Project photo={tasktrckr} cardTitle="Task Tracker" sourceCode="https://github.com/MohamedH98/kiqs" liveDemo="https://tasktrckr.netlify.app//" 
-        cardText="This a React task tracker app that allows users to create/delete tasks and toggle whether they want reminders for the tasks or not"/>
-        <Project photo={mapty} cardTitle="Mapty" sourceCode="https://github.com/MohamedH98/Mapty" liveDemo="https://trackmyworkout.netlify.app/" 
-        cardText="Mapty is an application that enables users to document their
-        workouts (run/cycle), record the distance, time, and
-        cadence/elevation gain. Their input is stored locally using
-        local storage. *desktop only*"/>
+              cardText="This a React task tracker app that allows users to create/delete tasks and toggle whether they want reminders for the tasks or not."/>
+              <Project photo={mapty} cardTitle="Mapty" sourceCode="https://github.com/MohamedH98/Mapty" liveDemo="https://trackmyworkout.netlify.app/" 
+              cardText="Mapty is an application that enables users to document their
+              workouts (run/cycle), record the distance, time, and
+              cadence/elevation gain. Their input is stored locally using
+              local storage. *desktop only*"/>
         <Project photo={forkify} cardTitle="Forkify" sourceCode="https://github.com/MohamedH98/forkify" liveDemo="https://forkify-website.netlify.app/" 
-        cardText="Forkify is an application that allows users to view the recipes
-        for meals they search for. The application also enables users to
-        upload new recipes and bookmark them too. *desktop only*"/>
+              cardText="Forkify is an application that allows users to view the recipes
+              for meals they search for. The application also enables users to
+              upload new recipes and bookmark them too. *desktop only*"/>
         <Project photo={papaya} cardTitle="Papaya" sourceCode="https://github.com/MohamedH98/Papaya" liveDemo="https://papaya-track.netlify.app/" 
-        cardText="A supply chain company that uses the blockchain."/>
+                cardText="A supply chain company that uses the blockchain."/>
 
       </div>
     </div>
